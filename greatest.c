78397e5c59f454cad6944dde8121832563838a17
Bhoomika262 <bhoomika.jalandhra@gmail.com>
2024-08-20 19:27:30 +0530

    Program to find greatest of four numbers entered by user. (TSRS)

---
+++ greatest.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int max_of_four(int a, int b, int c, int d)
+{
+    int max;
+    if(a>b)
+        max= a>c?a>d?a:d:c>d?c:d;
+    else
+        max= b>c?b>d?b:d:c>d?c:d;
+    return max;    
+}
+
+int main()
+{
+    int a,b,c,d;
+    printf("Enter four numbers: \n");
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d",ans);
+    return 0;
+    
+}
