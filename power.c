4abf82495979c2203b2d113e95f5073391d94fbc
Bhoomika262 <bhoomika.jalandhra@gmail.com>
2024-09-11 18:49:09 +0530

    Program to calculate power of a number using recursion in c language

--- power.c
+++ power.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
+#include<stdio.h>
 
 int exponent(int, int);
 
-int main() 
+int main()
 {
-    int n, m;
-    printf("Enter the base: ");
-    scanf("%d", &n);
-    printf("Enter the exponent: ");
-    scanf("%d", &m);
-    printf("%d", exponent(n, m));  
+    int n,m;
+    printf("Enter base: ");
+    scanf("%d",&n);
+    printf("Enter power: ");
+    scanf("%d",&m);
+    exponent(m,n); 
+    printf("%d", exponent(n,m));  
 }
 
-int exponent(int i, int j) {
-    if (j == 0) 
-    {
+int exponent(int i,int j)
+{
+    if (j == 0) {
         return 1;  
-    }
-     else 
+    } else 
     {
         return i * exponent(i, j - 1);  
     }
